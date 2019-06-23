1bf9369604e22717548122b85e7eed7d7eaf947e
maxgillham <maxfgillham@gmail.com>
2019-06-22 20:01:38 -0400

    basic template layout

--- server/app.py
+++ server/app.py
@@ -4,30 +4,23 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/index')
-"""
-This route is to render the homepage
-"""
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
 @app.route('/transcribe')
-"""
-This endpoint will activate the microphone, transcibe what is being said and return the text
-"""
 def transcribe():
     text = get_text()
     return text
 
-@app.route('/immerse__token')
-"""
-This will set up the token for the immersive reader veiw
-"""
+@app.route('/immerse_token')
 def immerse_token():
     response = get_immersive_reader_token()
     return response
 
+if __name__ == "__main__":
+    app.run(debug = True)
 
 
 

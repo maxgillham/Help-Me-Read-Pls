ad4756d891ee4190c4b62db568571b7ed6413536
maxgillham <maxfgillham@gmail.com>
2019-06-23 01:59:09 -0400

    keys for keyword enpoint added

--- server/keys.py
+++ server/keys.py
@@ -2,6 +2,8 @@ keys = {
         'speech_key': 'ENTER THE SPEECH KEY HERE',
         'IR_key' : 'ENTER THE IR KEY HERE',
         'IR_ENDPOINT' : 'ENTER THE IR ENDPOINT HERE',
+        'keyword_key': 'ENTER THE TEXT KEY HERE',
+        'keyword_url': 'ENTER THE TEXT URL HERE'
         }
 
 
